8339d322d5cc98fbf01a4c70720d1d535e0d0f9e
guoyw <qingyi_w@outlook.com>
2021-10-27 17:54:34 +0800

    add react exercise files

---
+++ React/03/my-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// 
+// const name = 'Josh Perez';
+// const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    element
+  </React.StrictMode>,
+  // element,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
