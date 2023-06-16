69321769c047797cac0b009a35443277d8a70fda
gywgithub <qingyi_w@outlook.com>
2023-06-16 11:02:27 +0800

    add Scripts

---
+++ Scripts/jianshu/index1.js
@@ -0,0 +1,44 @@
+// 简洁版
+// 获取所有文集
+fetch("https://www.jianshu.com/author/notebooks")
+  .then((res) => res.json())
+  .then((data) => {
+    // 输出所有文集
+    console.log(data);
+    let wenjiArr = [];
+    wenjiArr = data;
+    let articleLength = 0;
+    wenjiArr.forEach((item, index) => {
+      // 根据文集获取文章
+      fetch(`https://www.jianshu.com/author/notebooks/${item.id}/notes`)
+        .then((res2) => res2.json())
+        .then((data2) => {
+          console.log("输出文集下的所有文章：", data2);
+
+          console.log(item.name + " 文集中的文章数量: " + data2.length);
+          articleLength = articleLength + data2.length;
+
+          console.log("articleLength: ", articleLength);
+          data2.forEach(async (item2, i) => {
+            // 根据文章id获取Markdown内容
+            fetch(`https://www.jianshu.com/author/notes/${item2.id}/content`)
+              .then((res3) => res3.json())
+              .then((data3) => {
+                console.log(data3);
+
+                const blob = new Blob([data.content], {
+                  type: "text/markdown",
+                });
+
+                const link = document.createElement("a");
+
+                link.href = window.URL.createObjectURL(blob);
+
+                link.download = item.name + " - 《" + item2.title + `》.md`;
+
+                link.click();
+              });
+          });
+        });
+    });
+  });
