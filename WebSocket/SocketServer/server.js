5ceb9a6f29f2fee1a1209d87e934a7a762072a31
gywgithub <qingyi_w@outlook.com>
2024-04-25 17:28:29 +0800

    update socket file

--- WebSocket/SocketServer/server.js
+++ WebSocket/SocketServer/server.js
@@ -13,19 +13,10 @@ wss.on('connection', (ws) => {
         console.log('received: %s', message);
         // 在这里处理接收到的消息
 
-        // ws.send(JSON.stringify({
-        //     rend: 8090,
-        //     msg: '8090 client',
-        //     receive: 'test string'
-        // }));
-        console.log(typeof message)
-        console.log(typeof JSON.stringify(message))
-
         // 注意！！！
         // 这里面的数据要使用 toString() 转成字符串
         // 否则客户端接收的是个 blob 对象，将无法正确解析数据
         console.log(message.toString())
-        // ws.send(message.toString())
 
         broadcast(message.toString())
     });
